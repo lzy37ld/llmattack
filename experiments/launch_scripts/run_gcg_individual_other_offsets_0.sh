cc69e31343fa20ee9988d246cd28a4e7fde24a4a
liao.629 <liao.629@cse-cnc196881s.coeit.osu.edu>
2023-11-24 18:56:54 +0000

    1

---
+++ experiments/launch_scripts/run_gcg_individual_other_offsets_0.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+set -x
+
+
+export WANDB_MODE=disabled
+
+# Optionally set the cache for transformers
+# export TRANSFORMERS_CACHE='YOUR_PATH/huggingface'
+
+export model=$1 # llama2 or vicuna
+export setup=$2 # behaviors or strings
+
+# Create results folder if it doesn't exist
+if [ ! -d "../results" ]; then
+    mkdir "../results"
+    echo "Folder '../results' created."
+else
+    echo "Folder '../results' already exists."
+fi
+
+for data_offset in 10 20 30 40 50 60 70 80 90
+do
+
+# n_steps=1000 is too time consuming.. try 100 first. Look at figure 2 in the paper
+    python -u ../main.py \
+        --config="../configs/individual_${model}.py" \
+        --config.attack=gcg \
+        --config.train_data="../../data/advbench/harmful_${setup}.csv" \
+        --config.result_prefix="../results_n_step_1000/individual_${setup}_${model}_gcg_offset${data_offset}" \
+        --config.n_train_data=10 \
+        --config.data_offset=$data_offset \
+        --config.n_steps=1000 \
+        --config.test_steps=250 \
+        --config.batch_size=128
+done
---
+++ experiments/launch_scripts/run_gcg_individual_other_offsets_200.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+set -x
+
+
+export WANDB_MODE=disabled
+
+# Optionally set the cache for transformers
+# export TRANSFORMERS_CACHE='YOUR_PATH/huggingface'
+
+export model=$1 # llama2 or vicuna
+export setup=$2 # behaviors or strings
+
+# Create results folder if it doesn't exist
+if [ ! -d "../results" ]; then
+    mkdir "../results"
+    echo "Folder '../results' created."
+else
+    echo "Folder '../results' already exists."
+fi
+
+for data_offset in 220 230 240 250 260 270 280 290
+# for data_offset in 220 230 240 250 260 270 280 290
+do
+
+
+# n_steps=1000 is too time consuming.. try 100 first. Look at figure 2 in the paper
+    python -u ../main.py \
+        --config="../configs/individual_${model}.py" \
+        --config.attack=gcg \
+        --config.train_data="../../data/advbench/harmful_${setup}.csv" \
+        --config.result_prefix="../results_n_step_1000/individual_${setup}_${model}_gcg_offset${data_offset}" \
+        --config.n_train_data=10 \
+        --config.data_offset=$data_offset \
+        --config.n_steps=1000 \
+        --config.test_steps=250 \
+        --config.batch_size=128
+
+done
